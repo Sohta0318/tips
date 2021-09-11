d0e75fa8b3e661f3384942d80df38b7cb0f8c46b
Sohta0318 <nicosohta0318@gmail.com>
2021-09-11 15:16:41 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const o = [
+  {
+    name: "sohta",
+    age: 22,
+    country: "japan",
+  },
+  {
+    name: "sota",
+    age: "20",
+    country: "japan",
+  },
+];
+console.log(o);
+const [i, p] = o;
+console.log(i, p);
