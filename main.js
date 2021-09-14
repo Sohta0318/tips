9e0eff73b2f7a3a27d101933daa7083e13e6529d
Sohta0318 <nicosohta0318@gmail.com>
2021-09-13 17:19:09 -0700

    add max

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// what is [] mean in object array
 const o = [
   {
     name: "sohta",
@@ -11,9 +12,23 @@ const o = [
   },
 ];
 console.log(o);
+// const [i] = o //only take first one
 const [i, p] = o;
 console.log(i, p);
 
 // same as [k]
 const k = o[0];
 console.log(k);
+
+//  find max number in array
+const array = [1, 5, 13, 33, 66, 56];
+let max = array[0];
+for (let i = 0; i <= array.length; i++) {
+  if (max < array[i]) {
+    // max = array[i];
+    max = array[i];
+    //   } else {
+    //     console.log(max);
+  }
+}
+console.log(max);
